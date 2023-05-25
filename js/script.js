755e3352f5ac20511eff73a7e9cc9e9ad3611e79
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-05-26 00:21:29 +0300

    lesson 11

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-"use strict";
+// "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
+// number = 10;
 
-console.log(number);
+// console.log(number);
 
-const obj = {
-  a: 50,
-  b: 30,
-}
+// const obj = {
+//   a: 50,
+//   b: 30,
+// }
 
-obj.a = 90;
+// obj.a = 90;
 
-console.log(obj);
+// console.log(obj);
+
+const vechicleBodyWidth = 5000;
+const vechicleBodyLength = 4000;
+console.log("Ширина кузова автомобиля: " + vechicleBodyWidth + ", длина: " + vechicleBodyLength);
+
+// ['retert', 'aawefawef', 'awrawer'].map(a => )
+
+// Request
+// data
+// response
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase 
+
+let COLOR_RED = '#F00';
+
+// _spiKey
