dcca3c0bc56538262ab900f08e9e738303d73bb3
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-06-01 22:39:55 +0300

    lesson 16

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 "use strict";
 
+const isChecked = true,
+  isClose = false;
 
-const answers = [];
-answers[0] = prompt('What is your first name?', '');
-answers[1] = prompt('What is your last name?', '');
-answers[2] = prompt('How old are you?', '');
-
-
-console.log(`My name is ${answers[0]} last name is ${answers[1]} and I'm ${answers[2]} years old`);
+console.log(isChecked && !isClose);
