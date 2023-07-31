c9d63888e1fd44467da02e852d4128cc73b88f0d
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-07-31 08:55:06 +0300

    lesson 71

--- part2/Food_dist/js/script.js
+++ part2/Food_dist/js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-  
+
   // Tabs
 
   const tabs = document.querySelectorAll('.tabheader__item'),
@@ -45,12 +45,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const deadline = '2023-11-24';
 
-  function getTimeRemaining(endtime){
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 * 24)),
-          hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-          minutes = Math.floor((t / 1000 / 60) % 60),
-          seconds = Math.floor((t / 1000) % 60);
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24)),
+        hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+    }
+
     return {
       'total': t,
       'days': days,
@@ -60,38 +70,38 @@ window.addEventListener('DOMContentLoaded', () => {
     };
   }
 
-  function getZero(num){
-    if (num >= 0 && num < 10){
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
       return `0${num}`;
     } else {
       return num;
     }
   }
 
-  function setClock(selector, endtime){
+  function setClock(selector, endtime) {
     const timer = document.querySelector(selector),
-          days = timer.querySelector('#days'),
-          hours = timer.querySelector('#hours'),
-          minutes = timer.querySelector('#minutes'),
-          seconds = timer.querySelector('#seconds'),
-          timeInterval = setInterval(updateClock, 1000);
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
 
     updateClock();
 
-    function updateClock(){
+    function updateClock() {
       const t = getTimeRemaining(endtime);
 
-        days.innerHTML = getZero(t.days);
-        hours.innerHTML = getZero(t.hours);
-        minutes.innerHTML = getZero(t.minutes);
-        seconds.innerHTML = getZero(t.seconds);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
 
-        if (t.total <= 0){
-          clearInterval(timeInterval);
-        }
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
 
-     }
+    }
   }
 
   setClock('.timer', deadline);
