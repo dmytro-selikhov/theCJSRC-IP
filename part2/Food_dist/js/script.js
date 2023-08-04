cbff48518603142ced63bfcf871b1dad2aca167c
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-08-04 10:15:22 +0300

    lesson 80

--- part2/Food_dist/js/script.js
+++ part2/Food_dist/js/script.js
@@ -146,7 +146,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 5000);
+  // const modalTimerId = setTimeout(openModal, 5000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight > document.documentElement.scrollHeight - 1) {
@@ -157,46 +157,72 @@ window.addEventListener('DOMContentLoaded', () => {
 
   window.addEventListener('scroll', showModalByScroll);
 
+
+  // Используем классы для карточек
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector);
+      this.transfer = 37;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = +this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+        <div class="menu__item">
+          <img src=${this.src} alt=${this.alt} />
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
+        </div>
+      `;
+      this.parent.append(element);
+    }
+  }
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    'Меню "Фитнес"',
+    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+    9,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    "img/tabs/elite.jpg",
+    "elite",
+    'Меню "Премиум"',
+    'В меню "Премиум" мы используем не только красивый дизайн упаковки,но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    14,
+    '.menu .container'
+  ).render();
+
+  new MenuCard(
+    "img/tabs/post.jpg",
+    "post",
+    'Меню "Постное"',
+    'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    21,
+    '.menu .container'
+  ).render();
+
+
 });
 
-
-
-
-// 65. Скрипты и время их выполнения. setTimeout и setInterval
-
-// const timerId = setTimeout(function () {
-//   console.log('Hello!');
-// }, 2000);
-
-
-// lesson 74. (д) MutationObserver, ResizeObserver и contenteditable
-
-// lesson 75. Функции-конструкторы
-
-function User(name, id) {
-  this.name = name;
-  this.id = id;
-  this.human = true;
-  this.hello = function () {
-    console.log(`Hello ${this.name}`);
-  }
-}
-
-User.prototype.exit = function (name) {
-  console.log(`Пользователь ${this.name} ушел.`);
-}
-
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
-
-ivan.exit();
-alex.exit();
-
-ivan.hello();
-alex.hello();
-
-console.log(ivan);
-console.log(alex);
 
 
 
--- part2/MutationObserver/script.js
+++ part2/MutationObserver/script.js
@@ -11,3 +11,43 @@ observer.observe(box, {
 })
 
 observer.disconnect();
+
+
+
+// 65. Скрипты и время их выполнения. setTimeout и setInterval
+
+// const timerId = setTimeout(function () {
+//   console.log('Hello!');
+// }, 2000);
+
+
+// lesson 74. (д) MutationObserver, ResizeObserver и contenteditable
+
+// lesson 75. Функции-конструкторы
+
+// function User(name, id) {
+//   this.name = name;
+//   this.id = id;
+//   this.human = true;
+//   this.hello = function () {
+//     console.log(`Hello ${this.name}`);
+//   }
+// }
+
+// User.prototype.exit = function (name) {
+//   console.log(`Пользователь ${this.name} ушел.`);
+// }
+
+// const ivan = new User('Ivan', 28);
+// const alex = new User('Alex', 20);
+
+// ivan.exit();
+// alex.exit();
+
+// ivan.hello();
+// alex.hello();
+
+// console.log(ivan);
+// console.log(alex);
+
+
