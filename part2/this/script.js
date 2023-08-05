1d5760f0284b08a6dd7883c4d5177f6b13cd3584
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2023-08-05 20:28:12 +0300

    lesson 81

--- part2/this/script.js
+++ part2/this/script.js
@@ -95,34 +95,36 @@
 // console.log(double(4));
 
 
+
+//////////////////////////
 // lesson 77. Классы (ES6)
 
-class Rectangle {
-  constructor(height, width) {
-    this.height = height;
-    this.width = width;
-  }
+// class Rectangle {
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
 
-  calcArea() {
-    return this.height * this.width;
-  }
-}
+//   calcArea() {
+//     return this.height * this.width;
+//   }
+// }
 
-class ColoredRectangleWithText extends Rectangle {
-  constructor(height, width, text, bgColor) {
-    super(height, width);
-    this.text = text;
-    this.bgColor = bgColor;
-  }
+// class ColoredRectangleWithText extends Rectangle {
+//   constructor(height, width, text, bgColor) {
+//     super(height, width);
+//     this.text = text;
+//     this.bgColor = bgColor;
+//   }
 
-  showMyProps() {
-    console.log(`Text: ${this.text}, color: ${this.bgColor}`);
-  }
-}
+//   showMyProps() {
+//     console.log(`Text: ${this.text}, color: ${this.bgColor}`);
+//   }
+// }
 
-const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
-console.log(div);
-console.log(div.calcArea());
+// const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+// console.log(div);
+// console.log(div.calcArea());
 
 // const square = new Rectangle(10, 10);
 // const long = new Rectangle(20, 100);
@@ -133,3 +135,23 @@ console.log(div.calcArea());
 
 
 
+
+
+
+// lesson 80. Rest оператор и параметры по умолчанию (ES6)
+
+
+const log = function(a, b, ...rest){
+  console.log(a, b, rest);
+}
+
+log('basic', 'rest', 'operator', 'usage');
+
+
+function calcOrDouble(number, basis){
+  basis = basis || 2;
+  console.log(number * basis);
+}
+
+calcOrDouble(3, 5);
+calcOrDouble(3);
